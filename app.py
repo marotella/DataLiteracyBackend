b10e0a0b3353978f3434ee616aa6c969543ed8c6
marotella <michelle.a.rotella@gmail.com>
2023-08-19 17:35:29 -0400

    Tables created

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, g
+import models
 
 DEBUG = True
 PORT = 8000
 
 ## Initialize site
 app = Flask(__name__)
+
+##Database connection:
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+    
+@app.after_request
+def after_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+    
 
 ## The default URL 
 @app.route('/')
@@ -13,5 +28,6 @@ def index():
 
 ## Run the app when the program starts
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)
     
---
+++ models.py
@@ -0,0 +1,48 @@
+from peewee import *
+import datetime
+from flask_login import UserMixin
+import os
+from playhouse.db_url import connect
+
+DATABASE = connect(os.environ.get('DATABASE_URL') or 'sqlite:///digitalliteracy.db')
+
+class User(Model, UserMixin):
+    username = CharField(unique=True)
+    email = CharField(unique = True)
+    password = CharField()
+    
+    class Meta:
+        database = DATABASE
+
+class Student(Model):
+    user = ForeignKeyField(User, backref='students')  # Establishing the one-to-many relationship
+    studentID = IntegerField(unique = True)
+    firstName = CharField()
+    lastName = CharField()
+    screenerScore = IntegerField()
+    decodingScore = IntegerField()
+    encodingScore = IntegerField()
+    
+    class Meta:
+        database = DATABASE
+    
+class PlacementCriteria(Model):
+    user = ForeignKeyField(User, backref='placement_criteria')  # Establishing the one-to-many relationship
+    interventionName = CharField(unique = True)
+    screenerScoreMax = IntegerField()
+    decodingScoreMax = IntegerField()
+    encodingScoreMax = IntegerField()
+    screenerScoreMin = IntegerField()
+    decodingScoreMin = IntegerField()
+    encodingScoreMin = IntegerField()
+    
+    class Meta:
+        database = DATABASE
+
+def initialize():
+    DATABASE.connect()
+    # DATABASE.drop_tables([Ingredient], safe=True)
+
+    DATABASE.create_tables([User, PlacementCriteria, Student], safe=True)
+    print("Connected to the database, tables created.")
+    DATABASE.close()
