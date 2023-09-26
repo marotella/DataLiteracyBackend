217536c0d454cf3192ea374eaa87ace5b221d1e0
marotella <michelle.a.rotella@gmail.com>
2023-09-25 22:18:01 -0400

    pseudocoding for criteria

--- resources/criteria.py
+++ resources/criteria.py
@@ -7,19 +7,19 @@ import models
 
 placementCriteria = Blueprint("criteria", __name__)
 
-#INDEX
+#INDEX - View all criteria created for a user
 @placementCriteria.route("/", methods=["GET"])
-@jwt_required
+@jwt_required #check to make sure a user is logged in
 def get_all_critera():
     payload=request.get_json()
-    current_user_id = get_jwt_identity()
+    current_user_id = get_jwt_identity()  
     try:
-        criteria = [model_to_dict(criteria) for criteria in current_user_id.placement_criteria]
+        criteria = [model_to_dict(criteria) for criteria in current_user_id.placement_criteria] #query for the criteria linked to the current user
         return jsonify(data=criteria, status={"code":200, "message": "Successfully retrieved criteria"})
     except Exception as e:
         return jsonify(data={}, status={"code": 400, "message": str(e)})
     
-## CREATE
+## CREATE allows the user to add new placement criteria for an intervention
 @placementCriteria.route("/", methods=["POST"])
 @jwt_required
 def create_criteria():
@@ -39,7 +39,7 @@ def create_criteria():
         )
         criteria_dict = model_to_dict(criteria)
         return jsonify(data=criteria_dict, status={"code": 201, "message":"Successfully created intervention criteria."})
-    except Exception as e:
+    except Exception as e: ## add in a check for the intervention already existing 
         return jsonify(data={}, status={"code":400, "message": str(e)})
     
         
@@ -50,14 +50,14 @@ def delete_criteria(criteria_id):
     current_user_id = get_jwt_identity()
 
     try:
-        criteria= models.PlacementCriteria.get((models.PlacementCriteria.id == criteria_id)&(models.PlacementCriteria.user == current_user_id))
+        criteria= models.PlacementCriteria.get((models.PlacementCriteria.id == criteria_id)&(models.PlacementCriteria.user == current_user_id)) #query for the criteria that alings to the current user
         criteria.delete_instance()
         return jsonify(data={}, status={"code":201, "message":"Successfully deleted criteria"})
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 404, "message":"Criteria not found."})
     
     
-## UPDATE
+## UPDATE the placement criteria for a given user
 @placementCriteria.route("/<int:criteria_id>", methods=["PUT"])
 @jwt_required
 def update_criteria(criteria_id):
